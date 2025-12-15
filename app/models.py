2c4e8f087966d86f3ce295da294771b85cd8d884
Rakib Hoseen <rh01828315879@gmail.com>
2025-12-15 09:21:34 +0600

    Add init file and models file

---
+++ app/models.py
@@ -0,0 +1,13 @@
+from app import db
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    status = db.Column(db.String(20), default="Pending")
+    
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(32), nullable=False)
+    
